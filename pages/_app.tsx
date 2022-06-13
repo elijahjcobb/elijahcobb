3ccf5644c13d04f12746d4d53bca9d35a0307b8e
Elijah Cobb <elijah.cobb@vercel.com>
2022-06-12 21:49:01 -0700

    fixed name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import "../styles/index.css";
-import { Shell } from '../components/Shell';
+import { Shell } from '../components/shell';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return <>
