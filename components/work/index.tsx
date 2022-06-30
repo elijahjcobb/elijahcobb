34ec7f51fcea888b0d619f39da05ffad171d3614
Elijah Cobb <elijah.cobb@vercel.com>
2022-06-29 20:02:42 -0700

    moveable progress

--- components/work/index.tsx
+++ components/work/index.tsx
@@ -3,6 +3,7 @@ import styles from "./index.module.css";
 import { POSITIONS } from "../../data";
 import type { PositionType } from "../../data/types";
 import { Position } from "./position";
+import cn from "classnames";
 
 export function Positions() {
 	const [selected, setSelected] = useState(0);
@@ -11,8 +12,11 @@ export function Positions() {
 		<div className={styles.card}>
 			<div className={styles.positions}>
 				{POSITIONS.map((position, i) => {
-					return <button className={styles.position + (selected === i ? " " + styles.active : "")} key={i} onClick={() => setSelected(i)}>{position.shortName ?? position.name}</button>
+					return <button className={cn(styles.position, { [styles.active]: selected === i })} key={i} onClick={() => setSelected(i)}>{position.shortName ?? position.name}</button>
 				})}
+			</div>
+			<div className={styles.line}>
+				<div className={styles.progress} style={{ transform: `translateY(${selected * 56}px)` }} />
 			</div>
 			<Position position={POSITIONS[selected] as PositionType} />
 		</div>
