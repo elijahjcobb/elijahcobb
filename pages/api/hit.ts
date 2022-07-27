d3209db075254e227cf7f08dd6fcffa32016772e
Elijah Cobb <elijah.cobb@vercel.com>
2022-07-26 23:18:39 -0700

    ...

--- pages/api/hit.ts
+++ pages/api/hit.ts
@@ -1,18 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import type { NextApiRequest, NextApiResponse } from "next";
-import { redis } from "../../data/redis";
+// import { redis } from "../../data/redis";
 import type {
   ErrorResponse,
   HitRequest,
   TrackResponse,
 } from "../../data/types";
 
-export default async (
+export default (
   req: NextApiRequest,
   res: NextApiResponse<TrackResponse | ErrorResponse>
 ) => {
   console.log("HI");
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const body = JSON.parse(req.body) as HitRequest;
   console.log(body);
   res.send({ message: "OK" });
