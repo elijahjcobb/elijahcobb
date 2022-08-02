5b3757797637e5dce490ad7c72b53c0ee0ac5b15
Elijah Cobb <elijah.cobb@vercel.com>
2022-08-01 17:58:03 -0700

    fixed ships

--- components/ships/index.tsx
+++ components/ships/index.tsx
@@ -1,6 +1,5 @@
 import type { Ship } from "../../data/types";
 import styles from "./index.module.css";
-import { GoRepoForked, GoStar } from "react-icons/go";
 
 export interface ShipsProps {
 	ships: Ship[]
@@ -10,28 +9,16 @@ export function Ships(props: ShipsProps) {
 	return <div className={styles.container}>
 		<h1>Ships</h1>
 		<div className={styles.table}>
-			<span className={styles.header}>Year</span>
-			<span className={styles.header}>Title</span>
-			<span className={styles.header}>Description</span>
-			<span className={styles.header}>Demo</span>
 			{props.ships.map(ship => {
-				return <>
+				return <a
+					className={styles.row}
+					href={`https://github.com/${ship.slug}`}
+					key={ship.slug}
+					rel="noopener" target='_blank'>
 					<span className={styles.date}>{(new Date(ship.createdAt)).getFullYear()}</span>
 					<span className={styles.name}>{ship.name}</span>
-					<span>{ship.description}</span>
-					{/* <span>{Object.keys(ship.languages).join(", ")}</span>
-					<div className={styles.stats}>
-						<div className={styles.stat}>
-							<GoStar />
-							<span>{ship.stars}</span>
-						</div>
-						<div className={styles.stat}>
-							<GoRepoForked />
-							<span>{ship.forks}</span>
-						</div>
-					</div> */}
-					<span>url</span>
-				</>
+					<span className={styles.description}>{ship.description}</span>
+				</a>
 			})}
 		</div>
 	</div>
