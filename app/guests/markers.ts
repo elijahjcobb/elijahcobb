30298c00e2813880b556b08068e1997fcbe70013
Elijah Cobb <elijahjcobb@gmail.com>
2023-11-30 14:12:08 -0800

    use postgres instead

--- app/guests/markers.ts
+++ app/guests/markers.ts
@@ -1,10 +1,5 @@
 "use server";
-import { createClient } from "@vercel/kv";
-
-const kv = createClient({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
-});
+import { sql } from "@vercel/postgres";
 
 export type Location = [number, number];
 
@@ -15,23 +10,10 @@ export interface Marker {
 
 const RESOLUTION = 3;
 
-const ROOT_KEY = "guests";
-export type Key = `${typeof ROOT_KEY}:${string}:${string}`;
-
-function locationToKey(location: Location): Key {
-  return `${ROOT_KEY}:${location[0].toFixed(RESOLUTION)}:${location[1].toFixed(
-    RESOLUTION
-  )}`;
-}
-
-function keyToLocation(key: string): Location {
-  const [_, lat, lng] = key.split(":");
-  return [parseFloat(lat), parseFloat(lng)];
-}
-
 export async function addLocation(location: Location): Promise<void> {
-  const key = locationToKey(location);
-  await kv.incr(key);
+  await sql`INSERT INTO visitors (lat, lng, time) VALUES (${location[0].toFixed(
+    RESOLUTION
+  )}, ${location[1].toFixed(RESOLUTION)}, ${Date.now()})`;
 }
 
 const MAX_SIZE = 0.2;
@@ -39,13 +21,16 @@ const MIN_SIZE = 0.05;
 const SIZE_MULTIPLIER = 0.005;
 
 export async function getMarkers(): Promise<Marker[]> {
-  const markers: Marker[] = [];
-  for await (const key of kv.scanIterator({ match: `${ROOT_KEY}:*` })) {
-    const size = (await kv.get<number>(key)) ?? 0;
-    markers.push({
-      location: keyToLocation(key),
-      size: Math.max(Math.min(size * SIZE_MULTIPLIER, MAX_SIZE), MIN_SIZE),
-    });
-  }
-  return markers;
+  const res =
+    await sql`SELECT lat, lng, COUNT(*) FROM Visitors GROUP BY lat, lng`;
+  return res.rows.map((row) => {
+    const { lat, lng, count } = row;
+    return {
+      location: [lat, lng],
+      size: Math.max(
+        Math.min(parseInt(count) * SIZE_MULTIPLIER, MAX_SIZE),
+        MIN_SIZE
+      ),
+    };
+  });
 }
