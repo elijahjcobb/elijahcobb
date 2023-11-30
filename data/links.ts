eb3ba120119b9c4954126d1b6eecc76fa344099c
Elijah Cobb <elijahjcobb@gmail.com>
2023-11-30 08:28:10 -0800

    update twitter icon

--- data/links.ts
+++ data/links.ts
@@ -1,4 +1,6 @@
-import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 import type { LinkType, SocialType } from "./types";
 
 export const REVALIDATE_DEFAULT = 30;
@@ -12,15 +14,15 @@ export const LINKS: LinkType[] = [
 
 export const SOCIAL: SocialType[] = [
   {
-    link: "https://github.com/elijahjcobb",
-    icon: FaGithubSquare,
+    link: "https://twitter.com/elijahjcobb",
+    icon: FaXTwitter,
   },
   {
     link: "https://www.linkedin.com/in/elijahjcobb/",
     icon: FaLinkedin,
   },
   {
-    link: "https://twitter.com/elijahjcobb",
-    icon: FaTwitterSquare,
+    link: "https://github.com/elijahjcobb",
+    icon: FaGithubSquare,
   },
 ];
