22205d7c2be12fc02fdc50b4cf2f5ec392390fd4
Elijah Cobb <elijahjcobb@gmail.com>
2023-12-02 14:59:32 -0800

    update env

--- app/blog/[slug]/view/route.ts
+++ app/blog/[slug]/view/route.ts
@@ -5,7 +5,7 @@ export const POST = async (
   _: NextRequest,
   { params }: { params: { slug: string } }
 ): Promise<NextResponse> => {
-  if (process.env.VERCEL_ENV !== "production")
+  if (process.env.VERCEL_ENV === "development")
     return NextResponse.json({ status: "skipped" });
   kv.incr(`views:blog:${params.slug}`);
   return NextResponse.json({ status: "ok" });
