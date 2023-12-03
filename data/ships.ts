4f718babb6775dd6036946c79ce1919ecf7f2420
Elijah Cobb <elijahjcobb@gmail.com>
2023-12-02 22:50:00 -0800

    update order in ships

--- data/ships.ts
+++ data/ships.ts
@@ -3,6 +3,6 @@ import { Ship } from "./types";
 
 export async function fetchShips(): Promise<Ship[]> {
   const { rows } =
-    await sql`SELECT * FROM Ships ORDER BY year DESC, trophy, wip ASC, name;`;
+    await sql`SELECT * FROM Ships ORDER BY year DESC, trophy DESC, wip ASC, name;`;
   return rows as Ship[];
 }
