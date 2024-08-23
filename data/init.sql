0817ec31fb28889a582b451e0c2a318640ddd07f
Elijah Cobb <elijah@elijahcobb.com>
2024-08-23 08:51:39 -0700

    sharp

--- data/init.sql
+++ data/init.sql
@@ -1,20 +1,20 @@
 CREATE TABLE positions (
 	id SERIAL PRIMARY KEY,
-	slug TEXT UNIQUE,
+	slug TEXT UNIQUE NOT NULL,
 	short_name TEXT,
 	name TEXT NOT NULL,
 	href TEXT NOT NULL,
+	position TEXT NOT NULL,
 	start_year INT NOT NULL,
 	start_month INT NOT NULL,
 	end_year INT,
 	end_month INT,
-	position TEXT,
 	tasks JSON
 );
 
 CREATE TABLE links (
 	id SERIAL PRIMARY KEY,
-	href TEXT,
+	href TEXT NOT NULL,
 	position_id SERIAL,
 	FOREIGN KEY (position_id) REFERENCES positions(id)
 );
